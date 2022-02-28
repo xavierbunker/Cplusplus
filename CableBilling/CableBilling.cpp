8a3fb284a50c43ce25fc5f1909fc248c236c4f22
xavierbunker <xavier.l.bunker@gmail.com>
2022-02-27 20:19:18 -0600

    C++

--- CableBilling/CableBilling.cpp
+++ CableBilling/CableBilling.cpp
@@ -7,8 +7,101 @@
 #include <iostream>
 #include <iomanip>
 
+using namespace std;
+
+//Named constants - residential 
+const double RES_BILL_PROC_FEES = 4.50; 
+const double RES_BASIC_SERV_COST = 20.50; 
+const double RES_COST_PREM_CHANNEL = 7.50;
+
+//Named constants - business customers 
+const double BUS_BILL_PROC_FEES = 15.00; 
+const double BUS_BASIC_SERV_COST = 75.00; 
+const double BUS_BASIC_CONN_COST = 5.00; 
+const double BUS_COST_PREM_CHANNEL = 50.00;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Variable declaration 
+    int accountNumber; 
+    char customerType; 
+    int numofPremChannels; 
+    int numofBasicServConn; 
+    double amountDue;
+
+    cout << fixed << showpoint; 
+    cout << setprecision(2);
+
+    cout << "This program computes a cable" 
+        << "bill." << endl;
+
+    cout << "Enter account number (an integer): "; 
+    cin >> accountNumber; 
+    cout << endl;
+
+    cout << "Enter customer type: "
+        << "R or r (Residential), "
+        << "B or b (Business): ";
+        cin >> customerType; 
+        cout << endl;
+
+        switch (customerType)
+        {
+        case 'r': 
+        case 'R': 
+            cout << "Enter the number"
+            << " of premium channels: "; 
+            cin >> numofPremChannels; 
+            cout << endl;
+
+            amountDue = RES_BILL_PROC_FEES
+                + RES_BASIC_SERV_COST 
+                + numofPremChannels *
+                RES_COST_PREM_CHANNEL;
+
+            cout << "Account number: "
+                << accountNumber
+                << endl; 
+            cout << "Amount due: $"
+                << amountDue
+                << endl; 
+            break;
+
+        case 'b': 
+        case 'B': 
+            cout << "Enter the number of basic"
+            << " service connections: "; 
+            cin >> numofBasicServConn;
+            cout << endl;
+
+            cout << "Enter the number"
+                << " of premium channels: "; 
+            cin >> numofPremChannels; 
+            cout << endl;
+
+            if (numofBasicServConn <= 10)
+                amountDue = BUS_BILL_PROC_FEES
+                + BUS_BASIC_SERV_COST + numofPremChannels *
+                BUS_COST_PREM_CHANNEL;
+            else
+                amountDue = BUS_BILL_PROC_FEES
+                + BUS_BASIC_SERV_COST
+                + (numofBasicServConn - 10) *
+                BUS_BASIC_CONN_COST
+                + numofPremChannels *
+                BUS_COST_PREM_CHANNEL;
+
+            cout << "Account number: "
+                << accountNumber << endl;
+            cout << "Amount due: $" << amountDue
+               << endl;
+            break;
+
+        default:
+            cout << "Invalid customer type." << endl;
+        }//end switch
+
+        return 0;
+
 }
 
--- CubeSwitch/CubeSwitch.cpp
+++ CubeSwitch/CubeSwitch.cpp
@@ -1,33 +1,38 @@
-// Your Name
-// Date:
-// Program Title:
-// Program Description:
+// Your Name: Xavier L. Bunker
+// Date: 2/24/2022
+// Program Title: Cube Switching Code
+// Program Description: Code that lets user pick the part a cube that want to find information on.
 
-#include <iostream>
-#include <string>
-#include <iomanip>
+#include <iostream> // Header
+#include <string> // Header
+#include <iomanip> //Header
 
 using namespace std;
 
 // Named constants
+// Number that cant be change by user
 const double SIDE = 6;
 
 int main()
 {
 
 	// Variable declaration
+	// Used to plug in numbers or data that is being stored
 	double e, Boxvolume;
 	double Surfacearea;
 	double OneSurfacearea;
+	double onePerimeterSide;
 
 	int choice;
 
 	//Program title and description for the user
+	//Tell the user what is going on before the perform the task
 
 	cout << "Cube Switching Code" << endl << endl;
 	cout << "You Get To Pick What Getting Solve On A Cube" << endl << endl;
 
 	// User input
+	// Ask the user what they want to do and then direct them to it
 
 	cout << "Choose the corresponding number for what task you want to complete." << endl;
 	cout << "1 - task #1 : Perimeter of one side" << endl;
@@ -37,42 +42,49 @@ int main()
 	cout << "Enter selected number here: ";
 	cin >> choice;
 
-	// Calculations
-
 	// Output to the screen
 
 	switch (choice)
 	{
 	case 1:
-		cout << "Perform task #1 : Perimeter of one side" << endl;
+		cout << "Perform task #1 : Perimeter of one side" << endl; // Tell what the user picked
+		cout << "Please Enter the Edge Number: " << endl; // Ask user for input
+		cin >> e;
+
+		onePerimeterSide = 4 * e; // Calculations
+
+		cout << "Perimeter Of One Side: " << onePerimeterSide << endl;
+
 		break;
 		
 	case 2:
-		cout << "Perform task #2 : Surface area of one side" << endl;
-		cout << "Please Enter the Edge Numer: " << endl;
+		cout << "Perform task #2 : Surface area of one side" << endl; // Tell what the user picked
+		cout << "Please Enter the Edge Numer: " << endl; // Ask user for input
 		cin >> e;
 
-		OneSurfacearea = e * e;
+		OneSurfacearea = e * e; // Calculations
+
+		cout << "Surface Area Of One Side: " << OneSurfacearea << endl;
 
 		break;
 
 	case 3:
-		cout << "Perform task #3 : Surface area of the entire box" << endl;
-		cout << "Please Enter the Edge Numer: " << endl;
+		cout << "Perform task #3 : Surface area of the entire box" << endl; // Tell what the user picked
+		cout << "Please Enter the Edge Numer: " << endl; // Ask user for input
 		cin >> e;
 
-		Surfacearea = SIDE * (e * e);
+		Surfacearea = SIDE * (e * e); // Calculations
 
 		cout << "Surface Area: " << Surfacearea << endl;
 
 		break;
 
 	case 4:
-		cout << "Perform task #4 : Volume of the box" << endl;
-		cout << "Please Enter the Edge Numer: " << endl;
+		cout << "Perform task #4 : Volume of the box" << endl; // Tell what the user picked
+		cout << "Please Enter the Edge Numer: " << endl; // Ask user for input
 		cin >> e;
 
-		Boxvolume = e * e * e;
+		Boxvolume = e * e * e; // Calculations
 
 		cout << "Box Volume: " << Boxvolume << endl;
 
